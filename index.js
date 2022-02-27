25453c43a95a2e20206d8fc15e7a52ecf7a03e8e
Firouzfar <firouzfar.sebastien@gmail.com>
2022-02-27 19:03:16 +0100

    Resolve the js and css problem / resoudre le probleme du js et css

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.querySelectorAll(".titleNav").forEach((titlesNav) => titlesNav.addEventListener("click", () =>{
-    if(titlesNav.parentNode.classList.contains("active")){
-        titlesNav.parentNode.classList.toggle("active");
+document.querySelectorAll(".titleNavBlock").forEach((titleNavBlock) => titleNavBlock.addEventListener("click", () =>{
+    if(titleNavBlock.parentNode.classList.contains("active")){
+        titleNavBlock.parentNode.classList.toggle("active");
     }else{
-        document.querySelectorAll(".item").forEach((titlesNav) => titlesNav.parentNode.classList.remove("active"))
-        titlesNav.parentNode.classList.add("active")
+        document.querySelectorAll(".titleNavBlock").forEach((titleNavBlock) => titleNavBlock.parentNode.classList.remove("active"))
+        titleNavBlock.parentNode.classList.add("active")
     }
 }))
